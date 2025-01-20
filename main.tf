f1abdd4edc916b51a3755e818c21e6a3b6588fb1
hemanth <hemanthtadikonda29@gmail.com>
2025-01-20 11:52:41 +0530

    User-Info-app  Iac_code

---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = var.vpc_id
+  tags        = { Name = "allow_tls" }
+
+  ingress {
+    description = "APP"
+    from_port   = var.app_port
+    to_port     = var.app_port
+    protocol    = "tcp"
+    cidr_blocks = var.app_ingress_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = [ "0.0.0.0/0" ]
+    ipv6_cidr_blocks = [ "::/0" ]
+  }
+
+}
+
+
+
